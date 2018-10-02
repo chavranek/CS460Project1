b085d533f42dbe5483cff46eb37d1804acfea84f
narmour <n.armour0@gmail.com>
2018-10-02 12:38:57 -0700

    initial commit

---
+++ Project1.cpp
@@ -0,0 +1,23 @@
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include "SetLimits.h"
+#include "SyntacticalAnalyzer.h"
+
+int main (int argc, char * argv[])
+{
+	if (argc < 2)
+	{
+		printf ("format: proj1 <filename>\n");
+		exit (1);
+	}
+	SetLimits ();
+	//SyntacticalAnalyzer parser (argv[1]);
+    LexicalAnalyzer lex(argv[1]);
+
+
+    lex.GetToken();
+ 
+
+	return 0;
+}
